943dc54053042a7f1066f4ec1ace6d5b27b48972
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2023-09-07 12:44:33 +0200

    first commit

---
+++ Python.py
@@ -0,0 +1,37 @@
+print("Hello, World")  #skriver ut text 
+
+print("Detta är en text")
+print("Framgången är inte nyckel till lycka. Lycka är nyckeln till frangång.")
+
+
+
+print("""Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+""")
+
+print("Hello\nDmytro") 
+
+print("Fy"+"17")
+
+print("Hej!\nHur mår du idag?\nVad har du för planer för helgen?")
+
+# Exempel:
+Dmytro = "Dmytro"
+print("Hej,", Dmytro) 
+
+
+
+# Absolut! Här är ett välkomstprogram där jag inkluderar mitt namn:
+"""
+mitt_namn = "Dmytro"
+ditt_namn = input("Vad är ditt namn? ")
+print("Hej", ditt_namn, "! Välkommen till programmet. Jag är", mitt_namn, "och jag är här för att hjälpa dig!")
+""" 
